e2264edf829270d49fa7dc5c4fe7470eb3633ef3
aziani00 <applehijau19.ptss@gmail.com>
2020-12-07 22:44:01 +0800

    first time upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$(function () {
+    $('#btnHtml').click(function () {
+        $('#html').slideToggle('slow');
+    });
+
+    $('#btnCss').click(function () {
+        $('#css').slideToggle('slow');
+    });
+
+    $('#btnJs').click(function () {
+        $('#js').slideToggle('slow');
+    });
+});
